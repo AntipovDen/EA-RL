d2492a905b78cfac4d9ced467839135cbc225c8f
dantipov <antipovden@yandex.ru>
2018-04-02 21:26:34 +0300

    new plots for mean values

--- populational/plots_mean.py
+++ populational/plots_mean.py
@@ -29,8 +29,10 @@ with open('data/earl_2p2n_xdkom_merged.txt', 'r') as f:
 with open('data/earl_2p2n_xdkomzm_merged.txt', 'r') as f:
     earl_2p2n_xdkomzm = [[float(s) for s in line.split()] for line in f.readlines()]
 
-earl_2p2n_xdkom_theory = [[((2 * e / (e - 1)) ** k / (2 * k) + 1) * n for n in range(20, 101, 10)] for k in range(2, 7)]
-earl_2p2n_xdkomzm_theory = [[((3 * e / (e - 1)) ** k / (2 * k) + 1) * n for n in range(20, 101, 10)] for k in range(2, 7)]
+earl_2p2n_xdkom_theory = [[n / k * (e / (e - 1) + (2 * e / (e - 1)) ** k / 2 + k + 450) for n in range(20, 101, 10)] for k in range(2, 7)]
+    #[[((2 * e / (e - 1)) ** k / (2 * k) + 1) * n for n in range(20, 101, 10)] for k in range(2, 7)]
+earl_2p2n_xdkomzm_theory = [[n / k * (5 * e / (3 * (e - 1)) + (3 ** k + 2 ** k) * (e / (e - 1)) ** k / 2 + 2 * k + 900) for n in range(20, 101, 10)] for k in range(2, 7)]
+    #[[((3 * e / (e - 1)) ** k / (2 * k) + 1) * n for n in range(20, 101, 10)] for k in range(2, 7)]
 
 n_range = list(range(20, 101, 10))
 
@@ -64,15 +66,15 @@ plt.semilogy(n_range, list(map(lambda x, y: 2 * x * y, earl_2p2n_xdkomzm[-1], n_
 plt.semilogy(n_range, list(map(lambda x, y: 2 * x * y, earl_2p2n_xdkomzm_theory[-1], n_range)), 'mx--', 
              label='$(2 + 2n)$-EA+RL (theory)')
 plt.xlabel('$n$, individual size')
-plt.ylabel('runtime,\nfitness evaluations')
+plt.ylabel('runtime, fitness\nevaluations')
 plt.title('XdivK+OneMax+ZeroMax')
 plt.xlim(15, 105)
 
 plt.subplots_adjust(wspace=0.4,
                     hspace=0.4,
                     left=0,
-                    right=0.9,
-                    top=0.4,
+                    right=0.8,
+                    top=0.27,
                     bottom=0)
 
 plt.savefig('pic/means.png',
